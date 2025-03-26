0033c3b374f5940c9cf1c0b6472ec2965ef139a7
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-26 15:25:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong","stop"];
+
+sounds.forEach((element)=>{
+	let btn = document.createElement("button");
+	btn.classList.add("btn");
+	btn.innerText = element;
+
+	btn.addEventListener('click',()=>{
+		if(element == "stop") stopAudio();
+		document.getElementById(element).play();
+	})
+
+	document.querySelector("#buttons").appendChild(btn);
+})
+
+function stopAudio(){
+	sounds.forEach((element)=>{
+		let song = document.getElementById(element);
+		song.pause();
+		song.currentTime = 0;
+	});
+}
